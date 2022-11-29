65a29b66e00349304a151a3b720626bd0483f6b2
Alexandre Lima <dev.alexandrelima@gmail.com>
2022-11-29 10:06:23 -0300

    1059~1067

---
+++ _1067/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int input = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i <= input; i++)
+        {
+            if(i%2 == 1)
+                System.Console.WriteLine(i);
+        }
+    }
+
+}
